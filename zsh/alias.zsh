9745f603199de67f2ba491042000179e34aa51db
mrmattwright <matt@mrmattwright.com>
2022-01-05 14:22:38 +1300

    added brewfile and zsh aliases

---
+++ zsh/alias.zsh
@@ -0,0 +1,19 @@
+
+# Use exa instead of ls
+alias l='exa -1a'
+alias ll='exa -lh --git'
+alias ls='exa'
+alias la='ls -a'
+alias lt='exa -lT --git'
+
+alias g='git'
+alias be='bundle exec'
+
+# Homebrew
+alias brewu='brew update && brew upgrade && brew cask upgrade'
+alias brewc='brew cleanup'
+alias bservl='brew services list'
+alias bservr='brew services restart'
+alias bservs='brew services start'
+alias bservx='brew services stop'
+
--- zshrc
+++ zshrc
@@ -1,3 +1,10 @@
+if [ -x /usr/libexec/path_helper ]; then
+  eval `/usr/libexec/path_helper -s`
+fi
+
+source ~/.zsh/alias.zsh
+
+
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
