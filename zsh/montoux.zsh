8b8d74bffdc7ad03a83a59ded005a7d6397607e9
mrmattwright <matt@mrmattwright.com>
2023-01-06 13:07:57 +1300

    named directories

--- zsh/montoux.zsh
+++ zsh/montoux.zsh
@@ -1,14 +1,18 @@
 alias mon="cd $HOME/projects/monty"
 alias docs="cd $HOME/projects/docs"
 alias kt="cd $HOME/projects/montoux-exec"
+
 alias wf="cd $HOME/projects/workflow"
-alias poc="cd $HOME/projects/stochastic-poc && poetry shell"
+hash -d wfd="$HOME/projects/workflow" 
  
 export LDFLAGS="-L/opt/homebrew/opt/sqlite/lib -L/opt/homebrew/opt/zlib/lib -L/opt/homebrew/opt/bzip2/lib -L/opt/homebrew/opt/sqlite/lib -L/opt/homebrew/opt/zlib/lib -L/opt/homebrew/opt/bzip2/lib"
 export CPPFLAGS="-I/opt/homebrew/opt/sqlite/include -I/opt/homebrew/opt/zlib/include -I/opt/homebrew/opt/bzip2/include -I/opt/homebrew/opt/sqlite/include -I/opt/homebrew/opt/zlib/include -I/opt/homebrew/opt/bzip2/include"
 export OPENBLAS="$(brew --prefix openblas)"
 
 export PATH=$PATH:$HOME/go/bin
+
+
+
 
 function __vault_profile {
     if [ "$AWS_VAULT" = "" ]; then
